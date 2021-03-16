90db012a6a05b086e54261d397f8c089827298d3
AZiz <q.aziz@campus.tu-berlin>
2021-03-16 11:30:44 +0430

    blog-app added.

---
+++ blog-app/app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var path = require('path');
+var db = require('./mongoosedb.js');
+var Post = require('./models/post.js')
+
+var app = express();
+
+/* A template engine enables you to use static template files in your application. At runtime, the template engine replaces variables in a template file with actual values, and transforms the template into an HTML file sent to the client. */
+
+app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+
+app.get('/', function(req, res){
+  
+  Post.find((err, posts)=>{
+      if(err){
+        console.log('Error getting post');
+      }
+
+      console.log(posts);
+      res.render('index', {
+        posts: posts
+      })
+  })
+  
+});
+
+app.get('/about', function(req, res){
+  res.render('about')
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact')
+});
+
+
+
+
+
+app.listen(8888);
